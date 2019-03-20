6a10b299e16d00042b72b292b5d3edb0ba8a8f20
Anthony P. Austin <apaustin@vt.edu>
2019-03-20 11:24:25 -0400

    stokes:  Fix a few multigrid options; print multigrid report.

--- solvers/stokes/src/stokesPreconditionerSetup.c
+++ solvers/stokes/src/stokesPreconditionerSetup.c
@@ -116,16 +116,12 @@ static void stokesSchurComplementBlockDiagPreconditionerSetup(stokes_t *stokes, 
   ellipticOptions.setArgs("DEBUG ENABLE REDUCTIONS", "1");
   ellipticOptions.setArgs("KRYLOV SOLVER", "PCG");
   ellipticOptions.setArgs("PRECONDITIONER", "MULTIGRID");
-  //  ellipticOptions.setArgs("PRECONDITIONER", "FULLALMOND");
-  //  ellipticOptions.setArgs("MULTIGRID COARSENING", "HALFDEGREES");
-  ellipticOptions.setArgs("MULTIGRID COARSENING", "ALLDEGREES");
+  ellipticOptions.setArgs("MULTIGRID COARSENING", "HALFDEGREES");
   ellipticOptions.setArgs("MULTIGRID SMOOTHER", "DAMPEDJACOBI+CHEBYSHEV");
-  //  ellipticOptions.setArgs("MULTIGRID SMOOTHER", "DAMPEDJACOBI");
   ellipticOptions.setArgs("MULTIGRID CHEBYSHEV DEGREE", "2");
   ellipticOptions.setArgs("PARALMOND AGGREGATION STRATEGY", "DEFAULT");
   ellipticOptions.setArgs("PARALMOND CYCLE", "KCYCLE");
   ellipticOptions.setArgs("PARALMOND SMOOTHER", "CHEBYSHEV+DAMPEDJACOBI");
-  //  ellipticOptions.setArgs("PARALMOND SMOOTHER", "DAMPEDJACOBI");
   ellipticOptions.setArgs("PARALMOND CHEBYSHEV DEGREE", "2");
   ellipticOptions.setArgs("VERBOSE", "FALSE");
   elliptic->options = ellipticOptions;
@@ -141,6 +137,8 @@ static void stokesSchurComplementBlockDiagPreconditionerSetup(stokes_t *stokes, 
    * some scratch arrays with the stokes_t.
    */
   ellipticSolveSetup(elliptic, 0.0, kernelInfoV);
+
+  parAlmond::Report(elliptic->precon->parAlmond);
 
   stokes->precon = new stokesPrecon_t();
   stokes->precon->elliptic = elliptic;
